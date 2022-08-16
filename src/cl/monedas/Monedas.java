add7512375473ef6b62a4d64ad2ec290abafa067
Daniel Gomez <dgomez.informatica@gmail.com>
2022-08-15 21:14:41 -0400

    Se crea valores fijos de las monedas sacado por internet con fecha 2022-08-15

---
+++ src/cl/monedas/Monedas.java
@@ -0,0 +1,20 @@
+package cl.monedas;
+
+/**
+ * @author Daniel Gómez
+ * Contacto: dgomez.informatica@gmail.com
+ */
+
+public class Monedas {
+    
+    /**
+     * Estas valores fueron sacados desde internet, con fecha de 15-08-2022
+     * Estos son valores aproximados, que pueden cambiar
+     */
+    public final double PRECIO_DOLAR = 881.01;
+    public final double PRECIO_EURO = 895.38;
+    public final double PRECIO_LIBRAS_ESTERLINAS = 1061.06;
+    public final double PRECIO_YEN_JAPONES = 6.62;
+    public final double PRECIO_WON_SURCOREANO = 49991.29;
+   
+}
