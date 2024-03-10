96acb0c524200059a4e1262ca6e0a3fc0e07494b
Daniel Gomez <dgomez.informatica@gmail.com>
2024-03-09 21:54:58 -0300

    tasa de conversion de monedas, con valores actualizados al año 2024

---
+++ src/com/alura/conversor/moneda/TasaConversion.java
@@ -0,0 +1,15 @@
+package com.alura.conversor.moneda;
+
+public class TasaConversion {
+
+    /**
+     * Estos valores son estaticos y con valores fijos a la año actual 2024, para que sean
+     * utilizados en el programa de conversion de monedas. Para obtener los valores
+     * dinamicos se debe hacer una consulta a una API de conversion de monedas.
+     */
+    public static final double TASA_CONVERSION_DOLAR = 961.64;
+    public static final double TASA_CONVERSION_EURO = 1052.07;
+    public static final double TASA_CONVERSION_LIBRA = 1235.83;
+    public static final double TASA_CONVERSION_YEN = 6.54;
+    public static final double TASA_CONVERSION_WON_COREANO = 0.73;
+}
