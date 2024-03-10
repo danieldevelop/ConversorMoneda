a4cfaf3818ac70c1e0a436aaae4d1970098921e3
Daniel Gomez <dgomez.informatica@gmail.com>
2024-03-09 21:59:30 -0300

    clase de ejecucion del proyecto

---
+++ src/com/alura/conversor/Main.java
@@ -0,0 +1,10 @@
+package com.alura.conversor;
+
+import com.alura.conversor.views.FrmHome;
+
+public class Main {
+    public static void main(String[] args) {
+        FrmHome start = new FrmHome();
+        start.setVisible(true);
+    }
+}
